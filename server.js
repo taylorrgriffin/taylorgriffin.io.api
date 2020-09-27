f44498b009c660943a51e5ea4e780dd447bc6cf8
Taylor Griffin <taylor.griffin.365@gmail.com>
2020-09-26 23:00:26 -0700

    Added get endpoint to fetch PNGs

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
-const https = require('https');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser');
@@ -9,9 +8,6 @@ const { exec } = require('child_process');
 
 const app = express();
 const port = process.env.PORT || 8080;
-
-// import apiKey from secrets file
-const apiKey = require('./secrets.json').apiKey;
 
 // middleware config
 app.use(
@@ -37,14 +33,8 @@ const deleteFiles = (files, callback) => {
   });
 }
 
-// enforce valid API key
-app.use('/', (req, res, next) => {
-  if (req.query && req.query.apiKey == apiKey) {
-    next();
-  }
-  else {
-    res.status(401).send('Invalid API key.');
-  }
+app.get('/python-ast/:code', (req, res) => {
+  res.sendFile(path.resolve(path.resolve(__dirname,`./python-ast/images/${req.params.code}.png`)));
 });
 
 app.post('/python-ast', (req, res) => {
@@ -65,28 +55,17 @@ app.post('/python-ast', (req, res) => {
             res.status(500).send(`Error running parse on ${code}.py`);
           }
           else {
-            exec(`dot -Tpng -o${code}.png ./${code}.gv`, (err, stdout, stderr) => {
+            exec(`dot -Tpng -o./python-ast/images/${code}.png ./${code}.gv`, (err, stdout, stderr) => {
               if (err) {
                 console.error(err);
                 res.status(500).send(`Error converting ${code}.gv to ${code}.png`);
               }
               else {
+                res.status(200).send(JSON.stringify({ code }));
                 deleteFiles([`${code}.py`, `${code}.gv`], (err) => {
                   if (err) {
                     console.error(err);
                     res.status(500).send(`Error deleteing ${code}.py and/or ${code}.gv`);
-                  }
-                  else {
-                    res.sendFile(`${code}.png`, { root: path.join(__dirname) }, (err) => {
-                      if (err) {
-                        console.error(err);
-                      }
-                      fs.unlink(`${code}.png`, (err) => {
-                        if (err) {
-                          console.error(err);
-                        }
-                      });
-                    });
                   }
                 })
               }
@@ -98,15 +77,11 @@ app.post('/python-ast', (req, res) => {
   }
 });
 
+// catch-all for 404
 app.get('*', (req, res) => {
   res.status(404);
 });
 
-// create server using ssl for https
-https.createServer({
-  key: fs.readFileSync('./cert/key.pem'),
-  cert: fs.readFileSync('./cert/cert.pem')
-}, app)
-.listen(port, () => {
+app.listen(port, 'localhost', () => {
   console.info(`== Server listening on port ${port}`);
 });
